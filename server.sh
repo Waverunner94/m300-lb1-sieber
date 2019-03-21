6b6c3be611dd4548051a7b6bd7f39b09ebdcf82a
Waverunner94 <severin.sieber@edu.tbz.ch>
2019-03-21 14:15:45 +0100

    Create server.sh

---
+++ server.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#	Datenbank installieren und Konfigurieren
+#
+
+ufw enable
+ufw allow from 192.168.69.49 to any http
+ufw allow from 192.168.40.1 to any port 22
+
+apt-get update -y
+apt-get install -y apache2
+apt install php-pear php-fpm php-dev php-zip php-curl php-xmlrpc php-gd php-mysql php-mbstring php-xml libapache2-mod-php -y
+
+service apache2 restart
