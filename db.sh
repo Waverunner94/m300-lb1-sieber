2b35e011a04cca4df5797c391187a4ea996c5b72
Waverunner94 <severin.sieber@edu.tbz.ch>
2019-03-21 14:15:41 +0100

    Create db.sh

---
+++ db.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+#	Datenbank installieren und Konfigurieren
+#
+
+
+ufw enable
+ufw allow from 192.168.49.50 to any port 3306
+ufw allow from 192.168.40.1 to any port 22
+
+apt update -y
+
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password secret_password'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password secret_password'
+
+sudo apt-get install -y mysql-server
+
+sudo sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
+
+mysql -uroot -psecret_password <<%EOF%
+	CREATE USER 'root'@'192.168.69.50' IDENTIFIED BY 'admin';
+	GRANT ALL PRIVILEGES ON *.* TO 'root'@'192.168.69.50';
+	FLUSH PRIVILEGES;
+%EOF%
+
+mysql -uroot -psecret_password <<%EOF%
+	create database if not exists data_set;
+	create user 'www-data'@'localhost' identified by 'pwd'; 
+	grant usage on *.* to 'www-data'@'192.168.69.50' identified by 'pwd';
+	grant all privileges on data_set.* to 'www-data'@'192.168.69.50';
+	flush privileges;
+	use data_set;
+	create table data ( id INT PRIMARY KEY AUTO_INCREMENT, data FLOAT );
+	insert into data(data) values ( 1.173 );
+	insert into data(data) values ( 2.345 );
+%EOF%
+
+
+sudo service mysql restart
